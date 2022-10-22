9d4b26cf1346784143e89854a9af09c6f9c7fc4d
JOHALDO <johaldo_oliver@hotmail.com>
2022-10-21 23:12:53 -0500

    main johaldo

--- app/src/main/java/com/example/ec2_moviles2grupo5/MainActivity.kt
+++ app/src/main/java/com/example/ec2_moviles2grupo5/MainActivity.kt
@@ -6,14 +6,49 @@ import android.os.Bundle
 import android.view.View
 import com.example.ec2_moviles2grupo5.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding : ActivityMainBinding
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id){
+            R.id.btnRegistro ->{
+                iraRegistro()
+            }
+
+            R.id.btnFormulario ->{
+                iraFormulario()
+            }
+
+            R.id.btnListarMedicina ->{
+                iraListadoMedicina()
+            }
+        }
+    }
+
+    fun iraRegistro(){
+        val intent = Intent (this, RegistroActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun iraFormulario(){
+        val intent = Intent( this, FormularioActivity::class.java)
+        startActivity(intent)
+
+    }
+
+    fun iraListadoMedicina(){
+        val intent = Intent( this, ListaLibroMedicinaActivity::class.java)
+        startActivity(intent)
+
     }
 
 
